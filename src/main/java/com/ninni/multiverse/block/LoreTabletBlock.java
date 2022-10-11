f7cc7daaedfe236f2e710acd646059ca7ff76f71
0rc1nus <mitchellctlui@gmail.com>
2022-10-11 20:43:11 +0800

    Lore Tablet Shape

--- src/main/java/com/ninni/multiverse/block/LoreTabletBlock.java
+++ src/main/java/com/ninni/multiverse/block/LoreTabletBlock.java
@@ -3,7 +3,7 @@ package com.ninni.multiverse.block;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Mirror;
@@ -16,15 +16,24 @@ import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 public class LoreTabletBlock extends Block implements SimpleWaterloggedBlock {
     public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+    private static final VoxelShape X_AXIS_SHAPE = Block.box(6.5D, 0.0D, 2.0D, 9.5D, 15.0D, 14.0D);
+    private static final VoxelShape Z_AXIS_SHAPE = Block.box(2.0D, 0.0D, 6.5D, 14.0D, 15.0D, 9.5D);
 
     public LoreTabletBlock(Properties properties) {
         super(properties);
         this.registerDefaultState(this.stateDefinition.any().setValue(WATERLOGGED, false).setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+        return blockState.getValue(FACING).getAxis() == Direction.Axis.X ? X_AXIS_SHAPE : Z_AXIS_SHAPE;
     }
 
     @Override
