f03676b03d6f75ee192739be97c56fb7fbee7ced
0rc1nus <mitchellctlui@gmail.com>
2022-10-11 12:57:00 +0800

    Added idling ticks

--- src/main/java/com/ninni/multiverse/entities/ai/MineTargettedBlockGoal.java
+++ src/main/java/com/ninni/multiverse/entities/ai/MineTargettedBlockGoal.java
@@ -5,6 +5,9 @@ import com.ninni.multiverse.entities.CobblestoneGolemEntity;
 import com.ninni.multiverse.entities.MultiverseEntityTypes;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.level.pathfinder.Path;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
 
 import java.util.EnumSet;
@@ -13,6 +16,7 @@ public class MineTargettedBlockGoal extends Goal {
     private final CobblestoneGolemEntity golem;
     private int miningTicks;
     private BlockPos pos;
+    private int idlingTicks;
 
     public MineTargettedBlockGoal(CobblestoneGolemEntity golem) {
         this.golem = golem;
@@ -30,8 +34,14 @@ public class MineTargettedBlockGoal extends Goal {
         if (this.miningTicks < 0) {
             return false;
         }
+        if (this.idlingTicks > 80) {
+            return false;
+        }
         if (this.golem.getMiningBlock().isPresent() && this.golem.level.getBlockState(this.pos).getBlock() != this.golem.getMiningBlock().get().getBlock()) {
             return false;
+        }
+        if (this.golem.getMiningBlock().isPresent() && this.golem.getMinePos() != null && this.golem.level.getBlockState(this.pos).is(this.golem.getMiningBlock().get().getBlock())) {
+            this.golem.level.destroyBlockProgress(this.golem.getId(), this.pos, -1);
         }
         return super.canContinueToUse();
     }
@@ -41,7 +51,6 @@ public class MineTargettedBlockGoal extends Goal {
         if (this.pos != null) {
             float speed = this.golem.level.getBlockState(this.pos).getDestroySpeed(this.golem.level, this.pos);
             this.miningTicks = (int) speed * 10;
-            System.out.println(this.miningTicks);
         }
     }
 
@@ -49,35 +58,50 @@ public class MineTargettedBlockGoal extends Goal {
     public void tick() {
         Vec3 vec3 = Vec3.atCenterOf(this.pos);
         Crackiness crackiness = this.golem.getCrackiness();
-        if (this.miningTicks >= 0) {
-            this.miningTicks--;
-        }
-        if (this.miningTicks > 2) {
-            int var3 = (30 / (this.miningTicks - 2));
-            int var4 = Math.min(var3, 9);
-            this.golem.level.destroyBlockProgress(this.golem.getId(), this.pos, var4);
-        }
-        if (this.pos.closerToCenterThan(this.golem.position(), 2) && this.miningTicks == 0) {
-            this.golem.level.destroyBlock(this.pos, true);
-            this.golem.level.destroyBlockProgress(this.golem.getId(), this.pos, -1);
-            if (crackiness.getId() == 3) {
-                if (golem.getRandom().nextInt(8) == 0)
-                    this.golem.becomeExhausted(MultiverseEntityTypes.EXHAUSTED_COBBLESTONE_GOLEM);
-            } else {
-                if (golem.getRandom().nextInt(8) == 0) this.golem.setCrackiness(crackiness.getId() + 1);
+        if (this.pos.closerToCenterThan(this.golem.position(), 2.5)) {
+            if (this.miningTicks >= 0) {
+                this.miningTicks--;
+            }
+            if (this.miningTicks > 2) {
+                int var3 = (30 / (this.miningTicks - 2));
+                int var4 = Math.min(var3, 9);
+                this.golem.level.destroyBlockProgress(this.golem.getId(), this.pos, var4);
+            }
+            if (this.miningTicks == 0) {
+                this.golem.level.destroyBlock(this.pos, true);
+                this.golem.level.destroyBlockProgress(this.golem.getId(), this.pos, -1);
+                if (crackiness.getId() == 3) {
+                    if (golem.getRandom().nextInt(8) == 0) {
+                        this.golem.becomeExhausted(MultiverseEntityTypes.EXHAUSTED_COBBLESTONE_GOLEM);
+                    }
+                } else {
+                    if (golem.getRandom().nextInt(8) == 0) {
+                        this.golem.setCrackiness(crackiness.getId() + 1);
+                    }
+                }
             }
         } else {
             if (this.pos.getY() > this.golem.getY()) {
                 this.golem.setJumping(true);
             }
-            this.golem.getNavigation().moveTo(vec3.x(), vec3.y(), vec3.z(), 1.4D);
-            this.golem.getLookControl().setLookAt(vec3);
+            BlockHitResult result = this.golem.level.clip(new ClipContext(this.golem.position(), vec3, ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, this.golem));
+            BlockPos walkPos = new BlockPos(vec3.relative(result.getDirection(), 0.51F));
+            vec3 = new Vec3(walkPos.getX(), walkPos.getY(), walkPos.getZ());
+            Path path = this.golem.getNavigation().createPath(walkPos, 0);
+            if (path != null && path.canReach()) {
+                this.golem.getNavigation().moveTo(path, 1.4D);
+                this.golem.getLookControl().setLookAt(vec3);
+            }
+            if (this.golem.getNavigation().isDone()) {
+                this.idlingTicks++;
+            }
         }
     }
 
     @Override
     public void stop() {
         this.golem.setMinePos(null);
+        this.idlingTicks = 0;
     }
 
 }
