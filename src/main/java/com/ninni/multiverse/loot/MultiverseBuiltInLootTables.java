449c5d1eeba8bb4fa6da5a832a8342d02cd0f8f8
0rc1nus <mitchellctlui@gmail.com>
2022-10-22 19:00:53 +0800

    Update MultiverseBuiltInLootTables.java

--- src/main/java/com/ninni/multiverse/loot/MultiverseBuiltInLootTables.java
+++ src/main/java/com/ninni/multiverse/loot/MultiverseBuiltInLootTables.java
@@ -1,25 +1,14 @@
 package com.ninni.multiverse.loot;
 
-import com.google.common.collect.Sets;
 import com.ninni.multiverse.Multiverse;
 import net.minecraft.resources.ResourceLocation;
 
-import java.util.Set;
-
 public class MultiverseBuiltInLootTables {
-    private static final Set<ResourceLocation> LOCATIONS = Sets.newHashSet();
 
     public static final ResourceLocation RAINBOW_SHEEP_RAINBOW = MultiverseBuiltInLootTables.register("entities/rainbow_sheep/rainbow");
 
     private static ResourceLocation register(String string) {
-        return MultiverseBuiltInLootTables.register(new ResourceLocation(Multiverse.MOD_ID, string));
-    }
-
-    private static ResourceLocation register(ResourceLocation resourceLocation) {
-        if (LOCATIONS.add(resourceLocation)) {
-            return resourceLocation;
-        }
-        throw new IllegalArgumentException(resourceLocation + " is already a registered built-in loot table");
+        return new ResourceLocation(Multiverse.MOD_ID, string);
     }
 
 }
